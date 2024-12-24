b86f4725e7a3ce33219f3ccca0be36fbba1badaa
ChelseaVPutri <chelseaputri563@gmail.com>
2024-12-24 08:56:59 +0700

    ini juga konflik

--- lib/old-template-files/riwayat.dart
+++ lib/old-template-files/riwayat.dart
@@ -1,40 +1,40 @@
-import 'package:flutter/material.dart';
-import 'riwayat-model.dart';
+// import 'package:flutter/material.dart';
+// import 'riwayat-model.dart';
 
-class HistoryPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    List<HistoryModel> history = HistoryModel.getHistory();
+// class HistoryPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // List<HistoryModel> history = HistoryModel.getHistory();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Riwayat"),
-        backgroundColor: Colors.green,
-      ),
-      body: history.isEmpty
-          ? Center(
-              child: Text(
-                "Tidak ada riwayat.",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            )
-          : ListView.builder(
-              itemCount: history.length,
-              itemBuilder: (context, index) {
-                var item = history[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: ListTile(
-                    leading: Icon(Icons.history, color: Colors.green),
-                    title: Text(item.serviceType),
-                    subtitle:
-                        Text("Tanggal: ${item.date}\nBerat: ${item.weight} kg"),
-                    trailing: Text("Rp ${item.cost}",
-                        style: TextStyle(color: Colors.green)),
-                  ),
-                );
-              },
-            ),
-    );
-  }
-}
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Riwayat"),
+//         backgroundColor: Colors.green,
+//       ),
+//       // body: history.isEmpty
+//           ? Center(
+//               child: Text(
+//                 "Tidak ada riwayat.",
+//                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//               ),
+//             )
+//           : ListView.builder(
+//               itemCount: history.length,
+//               itemBuilder: (context, index) {
+//                 var item = history[index];
+//                 return Card(
+//                   margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+//                   child: ListTile(
+//                     leading: Icon(Icons.history, color: Colors.green),
+//                     title: Text(item.serviceType),
+//                     subtitle:
+//                         Text("Tanggal: ${item.date}\nBerat: ${item.weight} kg"),
+//                     trailing: Text("Rp ${item.cost}",
+//                         style: TextStyle(color: Colors.green)),
+//                   ),
+//                 );
+//               },
+//             ),
+//     );
+//   }
+// }
